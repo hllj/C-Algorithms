4d1a7027088de5c0f7fcc7736b56e4ba543e47ab
hlly <vanhop3499@gmail.com>
2018-03-04 10:24:17 +0700

    update reverse with Recursion for Linked List

--- Data Structure/LinkedList.cpp
+++ Data Structure/LinkedList.cpp
@@ -1,69 +1,81 @@
 #include <stdio.h>
+#include <conio.h>
 struct Node {
-    int info;
-    Node* pNext;
+	int info;
+	Node* pNext;
 };
 struct LinkedList {
-    Node* head;
-    LinkedList() {
-        head = NULL;
-    }
-    void insertNode(int value) {
-        Node* newNode = new Node;
-        newNode->info = value;
-        newNode->pNext = NULL;
-        if (head == NULL) head = newNode;
-        else {
-            Node* p;
-            p = head;
-            while (p->pNext != NULL) {
-                p = p->pNext;
-            }
-            p->pNext = newNode;
-        }
-    }
-    void displayList() {
-        Node* p;
-        p = head;
-        while (p != NULL) {
-            printf("%d ", p->info);
-            p = p->pNext;
-        }
-    }
-    void deleteNodeWithValue(int value) {
-        Node* p;
-        p = head;
-        while (p->pNext != NULL) {
-            if (p->pNext->info == value) {
-                Node* next = p->pNext->pNext;
-                delete(p->pNext);
-                p->pNext = next;
-            }
-            p = p->pNext;
-            if (p == NULL) break;
-        }
-    }
-    void reverse() {
-        Node* p;
-        p = head;
-        Node* prev = NULL;
-        while (p != NULL) {
-            Node* next = p->pNext;
-            p->pNext = prev;
-            prev = p;
-            p = next;
-        }
-        head = prev;
-    }
+	Node* head;
+	LinkedList() {
+		head = NULL;
+	}
+	void insertNode(int value) {
+		Node* newNode = new Node;
+		newNode->info = value;
+		newNode->pNext = NULL;
+		if (head == NULL) head = newNode;
+		else {
+			Node* p;
+			p = head;
+			while (p->pNext != NULL) {
+				p = p->pNext;
+			}
+			p->pNext = newNode;
+		}
+	}
+	void displayList() {
+		Node* p;
+		p = head;
+		while (p != NULL) {
+			printf("%d ", p->info);
+			p = p->pNext;
+		}
+	}
+	void deleteNodeWithValue(int value) {
+		Node* p;
+		p = head;
+		while (p->pNext != NULL) {
+			if (p->pNext->info == value) {
+				Node* next = p->pNext->pNext;
+				delete(p->pNext);
+				p->pNext = next;
+			}
+			p = p->pNext;
+			if (p == NULL) break;
+		}
+	}
+	void reverse() {
+		Node* p;
+		p = head;
+		Node* prev = NULL;
+		while (p != NULL) {
+			Node* next = p->pNext;
+			p->pNext = prev;
+			prev = p;
+			p = next;
+		}
+		head = prev;
+	}
+	void reverseWithRecursion(Node* p) {
+		if (p == NULL) return;
+		head = p;
+		reverseWithRecursion(p->pNext);
+		Node* next = p->pNext;
+		p->pNext = NULL;
+		if (next != NULL) next->pNext = p;
+	}
 };
 int main() {
-    LinkedList* myList = new LinkedList;
-    myList->insertNode(2);
-    myList->insertNode(3);
-    myList->insertNode(4);
-    myList->insertNode(3);
+	LinkedList* myList = new LinkedList;
+	myList->insertNode(2);
+	myList->insertNode(3);
+	myList->insertNode(4);
+	myList->insertNode(3);
     //myList->deleteNodeWithValue(3);
-    myList->reverse();
-    myList->displayList();
-    return 0;
+	myList->reverse();
+	myList->displayList();
+	printf("\n");
+	myList->reverseWithRecursion(myList->head);
+	myList->displayList();
+	return 0;
 }
